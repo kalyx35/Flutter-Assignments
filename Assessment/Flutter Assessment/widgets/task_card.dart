871bdbc3bcb639f74ddbfa1234a69d6d577029bb
Ananya <ananyaanu3125@gmail.com>
2025-08-11 22:29:03 +0530

    Initial commit

---
+++ Assessment/Flutter Assessment/widgets/task_card.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:student_connect/models/task.dart';
+
+class TaskCard extends StatelessWidget {
+  final Task task;
+  final VoidCallback onToggle;
+  final VoidCallback onEdit;
+  final VoidCallback onDelete;
+
+  const TaskCard({
+    super.key,
+    required this.task,
+    required this.onToggle,
+    required this.onEdit,
+    required this.onDelete,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 3,
+      margin: const EdgeInsets.symmetric(vertical: 6, horizontal: 12),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+      child: ListTile(
+        leading: Checkbox(
+          value: task.done,
+          onChanged: (_) => onToggle(),
+        ),
+        title: Text(task.title,
+            style: TextStyle(
+                decoration: task.done ? TextDecoration.lineThrough : null)),
+        subtitle: Text(task.description),
+        trailing: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            IconButton(icon: Icon(Icons.edit), onPressed: onEdit),
+            IconButton(icon: Icon(Icons.delete), onPressed: onDelete),
+          ],
+        ),
+      ),
+    );
+  }
+}
